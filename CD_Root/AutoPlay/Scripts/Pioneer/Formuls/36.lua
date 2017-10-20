67f46ca3a25d48c51125be99749c8d3ad498bbc1
emptyteam <32950909+emptyteam@users.noreply.github.com>
2017-10-20 21:36:49 +0330

    اضافه شدن نمایش عدد در قسمت اطلاعات

--- CD_Root/AutoPlay/Scripts/Pioneer/Formuls/36.lua
+++ CD_Root/AutoPlay/Scripts/Pioneer/Formuls/36.lua
@@ -29,8 +29,12 @@ if Findt0 ~= -1 and ListBox.GetItemData("unknowns", Findt0) == "t0" and ListBox.
 		Binesh.CreateHTMLFile("t<sub>0</sub> = <span>V<sub>0</sub>/a</span>")
 		t0 = (V0_Speed)/(a)
 		
-		Binesh.AddToHTML("t<sub>0</sub> = <span>"..V0.."/"..a.."</span> &#8658; t<sub>0</sub> = "..t0.."s")
+		Binesh.AddToHTML("t<sub>0</sub> = <span>"..V0.."/"..a.."</span> &#8658; t<sub>0</sub> = "..t0.." s")
 		Web.Refresh("Web1");
+		
+		Binesh.AddToHTML("V<sub>0</sub> = "..V0_Speed.." m/s","V")
+		Binesh.AddToHTML("a = "..a.." m/s<sup>2</sup>","V")
+		Binesh.AddToHTML("t<sub>0</sub> = "..t0.." s","V")
 		
 		unknownPersianindex = ListBox.FindItem("unknowns", -1, LBBYTEXT, "«Ê·Ì‰ “„«‰");
 		unknownSymbolindex = ListBox.FindItem("unknowns", -1, LBBYDATA, "t0");
@@ -46,6 +50,10 @@ elseif FindV0_Speed ~= -1 and ListBox.GetItemData("unknowns", FindV0_Speed) == "
 		Binesh.AddToHTML("V<sub>0</sub> = "..t0.."*"..a.." &#8658; V<sub>0</sub> = "..V0_Speed.."m/s")
 		Web.Refresh("Web1");
 		
+		Binesh.AddToHTML("t<sub>0</sub> = "..t0.." s","V")
+		Binesh.AddToHTML("a = "..a.." m/s<sup>2</sup>","V")
+		Binesh.AddToHTML("V<sub>0</sub> = "..V0_Speed.." m/s","V")
+		
 		unknownPersianindex = ListBox.FindItem("unknowns", -1, LBBYTEXT, "«Ê·Ì‰ ”—⁄ ");
 		unknownSymbolindex = ListBox.FindItem("unknowns", -1, LBBYDATA, "V0");
 		if unknownPersianindex == unknownSymbolindex and unknownPersianindex ~= -1 then
@@ -59,6 +67,10 @@ elseif Finda ~= -1 and ListBox.GetItemData("unknowns", Finda) == "a" and ListBox
 		
 		Binesh.AddToHTML("a = <span>"..V0_Speed.."/"..t0.."</span> &#8658; a = "..a.."m/s<sup>2</sup>")
 		Web.Refresh("Web1");
+		
+		Binesh.AddToHTML("V<sub>0</sub> = "..V0_Speed.." m/s","V")
+		Binesh.AddToHTML("t<sub>0</sub> = "..t0.." s","V")
+		Binesh.AddToHTML("a = "..a.." m/s<sup>2</sup>","V")
 		
 		unknownPersianindex = ListBox.FindItem("unknowns", -1, LBBYTEXT, "‘ «»");
 		unknownSymbolindex = ListBox.FindItem("unknowns", -1, LBBYDATA, "a");
