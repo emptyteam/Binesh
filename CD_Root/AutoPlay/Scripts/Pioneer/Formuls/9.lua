42e99b6c90cc52fb007eae9b55a8da8f5171a8e4
emptyteam <32950909+emptyteam@users.noreply.github.com>
2017-10-23 22:38:53 +0330

    حذف پرانتز های اضافه

--- CD_Root/AutoPlay/Scripts/Pioneer/Formuls/9.lua
+++ CD_Root/AutoPlay/Scripts/Pioneer/Formuls/9.lua
@@ -26,7 +26,7 @@ Findt = ListBox.FindItem("unknowns", -1, LB_BYTEXT, "“„«‰")
 if FindP_Power ~= -1 and ListBox.GetItemData("unknowns", FindP_Power) == "P" and ListBox.FindItem("Words", -1, LB_BYTEXT, " Ê«‰") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "P") ~= -1 then
 	if W ~= nil and t ~= nil then
 		Binesh.CreateHTMLFile("P = <span>W/t</span>")
-		P_Power = (W)/(t)
+		P_Power = W/t
 		
 		Binesh.AddToHTML("P = <span>"..W.."/"..t.."</span> &#8658; P = "..P_Power.." W")
 		Web.Refresh("Web1");
@@ -40,7 +40,7 @@ if FindP_Power ~= -1 and ListBox.GetItemData("unknowns", FindP_Power) == "P" and
 elseif FindW ~= -1 and ListBox.GetItemData("unknowns", FindW) == "W" and ListBox.FindItem("Words", -1, LB_BYTEXT, "ò«—") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "W") ~= -1 then
 	if P_Power ~= nil and t ~= nil then
 		Binesh.CreateHTMLFile("P = W/t", "W = Pt")
-		W = (P_Power)*(t)
+		W = P_Power*t
 		
 		Binesh.AddToHTML("W = "..P_Power.."*"..t.." &#8658; W = "..W.." J")
 		Web.Refresh("Web1");
@@ -54,7 +54,7 @@ elseif FindW ~= -1 and ListBox.GetItemData("unknowns", FindW) == "W" and ListBox
 elseif Findt ~= -1 and ListBox.GetItemData("unknowns", Findt) == "t" and ListBox.FindItem("Words", -1, LB_BYTEXT, "“„«‰") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "t") ~= -1 then
 	if P_Power ~= nil and W ~= nil then
 		Binesh.CreateHTMLFile("P = <span>W/t</span>", "t = <span>W/P</span>")
-		t = (W)/(P_Power)
+		t = W/P_Power
 		
 		Binesh.AddToHTML("t = <span>"..W.."/"..P_Power.."</span> &#8658; t = "..t.." s")
 		Web.Refresh("Web1");
