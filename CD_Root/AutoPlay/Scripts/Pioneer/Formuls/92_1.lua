12197580fbbb1a4afe98a0f8c4f95600a57f2a81
emptyteam <32950909+emptyteam@users.noreply.github.com>
2018-01-18 14:47:57 +0330

    رفع باگ
    بار آزمون با متغییر q_0_87 معرفی شده و اینجا q نوشته شده بود

--- CD_Root/AutoPlay/Scripts/Pioneer/Formuls/92_1.lua
+++ CD_Root/AutoPlay/Scripts/Pioneer/Formuls/92_1.lua
@@ -25,38 +25,38 @@ function Binesh.F92_1()
 	end
 	--------------------------END {DELTA}--------------------------
 	if _DeltaV_Electric == nil and ListBox.FindItem("Words", -1, LB_BYTEXT, "Ê· «é") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, " €ÌÌ— Ê· «é") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "Ê· «é  €ÌÌ—") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "DeltaV") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "V") ~= -1 then
-		if _DeltaU ~= nil and q ~= nil then
-			Binesh.CreateHTMLFile("DeltaV = <span>DeltaU/q</span>")
-			_DeltaV_Electric = _DeltaU/q
-			Binesh.AddToHTML("DeltaV = <span>".._DeltaU.."/"..q.."</span> &#8658; DeltaV = ".._DeltaV_Electric.." V")
+		if _DeltaU ~= nil and q_0_87 ~= nil then
+			Binesh.CreateHTMLFile("DeltaV = <span>DeltaU/q<sub>0</sub></span>")
+			_DeltaV_Electric = _DeltaU/q_0_87
+			Binesh.AddToHTML("DeltaV = <span>".._DeltaU.."/"..q_0_87.."</span> &#8658; DeltaV = ".._DeltaV_Electric.." V")
 			
 			Binesh.AddToHTML("DeltaU: ".._DeltaU.." J", "V")
-			Binesh.AddToHTML("q: "..q.." C", "V")
+			Binesh.AddToHTML("q<sub>0</sub>: "..q_0_87.." C", "V")
 			Binesh.AddToHTML("DeltaV: ?", "V")
 		end
 	end
 	
 	if _DeltaU == nil and ListBox.FindItem("Words", -1, LB_BYTEXT, " €ÌÌ— «‰—éÌ") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "«‰—éÌ  €ÌÌ—") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "«‰—éÌ Å «‰”Ì·") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "DeltaU") ~= -1 then
-		if _DeltaV_Electric ~= nil and q ~= nil then
-			Binesh.CreateHTMLFile("DeltaV = <span>DeltaU/q</span>", "DeltaU = DeltaVq")
-			_DeltaU = _DeltaV_Electric * q
-			Binesh.AddToHTML("DeltaU = ".._DeltaV_Electric.."*"..q.." &#8658; DeltaU = ".._DeltaU.." J")
+		if _DeltaV_Electric ~= nil and q_0_87 ~= nil then
+			Binesh.CreateHTMLFile("DeltaV = <span>DeltaU/q<sub>0</sub></span>", "DeltaU = DeltaVq<sub>0</sub>")
+			_DeltaU = _DeltaV_Electric * q_0_87
+			Binesh.AddToHTML("DeltaU = ".._DeltaV_Electric.."*"..q_0_87.." &#8658; DeltaU = ".._DeltaU.." J")
 			
 			Binesh.AddToHTML("DeltaV: ".._DeltaV_Electric.." V", "V")
-			Binesh.AddToHTML("q: "..q.." C", "V")
+			Binesh.AddToHTML("q<sub>0</sub>: "..q_0_87.." C", "V")
 			Binesh.AddToHTML("DeltaU: ?", "V")
 		end
 	end
 	
-	if q == nil and ListBox.FindItem("Words", -1, LB_BYTEXT, "–—Â") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "q") ~= -1 then
+	if q_0_87 == nil and ListBox.FindItem("Words", -1, LB_BYTEXT, "–—Â") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "q_0_87") ~= -1 then
 		if _DeltaV_Electric ~= nil and _DeltaU ~= nil then
-			Binesh.CreateHTMLFile("DeltaV = <span>DeltaU/q</span>", "q = <span>DeltaU/DeltaV</span>")
-			q = _DeltaU/_DeltaV_Electric
-			Binesh.AddToHTML("q = <span>".._DeltaU.."/".._DeltaV_Electric.."</span> &#8658; q = "..q.." C")
+			Binesh.CreateHTMLFile("DeltaV = <span>DeltaU/q<sub>0</sub></span>", "q<sub>0</sub> = <span>DeltaU/DeltaV</span>")
+			q_0_87 = _DeltaU/_DeltaV_Electric
+			Binesh.AddToHTML("q<sub>0</sub> = <span>".._DeltaU.."/".._DeltaV_Electric.."</span> &#8658; q<sub>0</sub> = "..q_0_87.." C")
 			
 			Binesh.AddToHTML("DeltaU: "..DeltaU.." J", "V")
 			Binesh.AddToHTML("DeltaV: ".._DeltaV_Electric.." V", "V")
-			Binesh.AddToHTML("q: ?", "V")
+			Binesh.AddToHTML("q<sub>0</sub>: ?", "V")
 		end
 	end
 end
