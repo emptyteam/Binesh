33836255788f7fed59f73e1658e5b8bb1182833a
emptyteam <32950909+emptyteam@users.noreply.github.com>
2018-01-19 22:58:19 +0330

    رفع باگ

--- CD_Root/AutoPlay/Scripts/Pioneer/Formuls/47_1.lua
+++ CD_Root/AutoPlay/Scripts/Pioneer/Formuls/47_1.lua
@@ -19,20 +19,20 @@ function Binesh.F47_1()
 			
 			Binesh.AddToHTML("N = <span>"..fs_max.."/"..Mus.."</span> &#8658; N = "..N.." N")
 			
-			Binesh.AddToHTML("f<sub>s,max</sub>: "..fs,max.." N", "V")
+			Binesh.AddToHTML("f<sub>s,max</sub>: "..fs_max.." N", "V")
 			Binesh.AddToHTML("Mu<sub>s</sub>: "..Mus, "V")
 			Binesh.AddToHTML("N: ?", "V")
 		end
 	end
 	
 	if Mus == nil and ListBox.FindItem("Words", -1, LB_BYTEXT, "÷—Ì» «’ÿò«ò «Ì” «ÌÌ") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "Mus") ~= -1 then
-		if fs_max ~= nil and Mus ~= nil then
+		if fs_max ~= nil and N ~= nil then
 			Binesh.CreateHTMLFile("f<sub>s,max</sub> = Mu<sub>s</sub>N", "Mu<sub>s</sub> = <span>f<sub>s,max</sub>/N</span>")
 			Mus = fs_max/N
 			
 			Binesh.AddToHTML("Mu<sub>s</sub> = <span>"..fs_max.."/"..N.."</span> &#8658; Mu<sub>s</sub> = "..Mus)
 			
-			Binesh.AddToHTML("f<sub>s,max</sub>: "..fs,max.." N", "V")
+			Binesh.AddToHTML("f<sub>s,max</sub>: "..fs_max.." N", "V")
 			Binesh.AddToHTML("N: "..N.." N", "V")
 			Binesh.AddToHTML("Mu<sub>s</sub>: ?", "V")
 		end
