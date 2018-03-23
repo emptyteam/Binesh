f983da0d975934fabc7c6361a0e612a819b87b33
emptyteam <32950909+emptyteam@users.noreply.github.com>
2018-03-23 13:20:11 +0430

    Add files via upload

--- CD_Root/AutoPlay/Scripts/Pioneer/Formuls/91_1.lua
+++ CD_Root/AutoPlay/Scripts/Pioneer/Formuls/91_1.lua
@@ -24,81 +24,83 @@ function Binesh.F91_1()
 		end
 	end
 	--------------------------END {DELTA}--------------------------
-	if _DeltaU == nil and ((ListBox.FindItem("Words", -1, LB_BYTEXT, " €ÌÌ— «‰—éÌ") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "«‰—éÌ  €ÌÌ—") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "«‰—éÌ Å «‰”Ì·") ~= -1) or (ListBox.FindItem("Words", -1, LB_BYTEXT, "DeltaU") ~= -1)) then
-		if E_Electric ~= nil and q ~= nil and Teta ~= nil and d ~= nil then
-			Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta")
-			_DeltaU = -E_Electric*Math.Abs(q)*d*Math.Cos(Math.Rad(Teta))
-			
-			Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
-			Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
-			Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
-			Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
-			Binesh.AddToHTML("DeltaU = ?", "V")
-			
-			Binesh.AddToHTML("DeltaU = -"..Binesh.ProgressNumber(E_Electric).."*|"..Binesh.ProgressNumber(q).."|*"..Binesh.ProgressNumber(d).."*Cos"..Binesh.ProgressNumber(Teta).." &#8658; DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J")
+	if ListBox.FindItem("Words", -1, LB_BYTEXT, "„€‰«ÿÌ”Ì") == -1 then
+		if _DeltaU == nil and ((ListBox.FindItem("Words", -1, LB_BYTEXT, " €ÌÌ— «‰—éÌ") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "«‰—éÌ  €ÌÌ—") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "«‰—éÌ Å «‰”Ì·") ~= -1) or (ListBox.FindItem("Words", -1, LB_BYTEXT, "DeltaU") ~= -1)) then
+			if E_Electric ~= nil and q ~= nil and Teta ~= nil and d ~= nil then
+				Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta")
+				_DeltaU = -E_Electric*Math.Abs(q)*d*Math.Cos(Math.Rad(Teta))
+				
+				Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
+				Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
+				Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
+				Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
+				Binesh.AddToHTML("DeltaU = ?", "V")
+				
+				Binesh.AddToHTML("DeltaU = -"..Binesh.ProgressNumber(E_Electric).."*|"..Binesh.ProgressNumber(q).."|*"..Binesh.ProgressNumber(d).."*Cos"..Binesh.ProgressNumber(Teta).." &#8658; DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J")
+			end
 		end
-	end
-	
-	if E_Electric == nil and (ListBox.FindItem("Words", -1, LB_BYTEXT, "„Ìœ«‰") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "E") ~= -1) then
-		if q ~= nil and _DeltaU ~= nil and d ~= nil and Teta ~= nil then
-			Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "E = - <span>DeltaU/|q|dCosTeta</span>")
-			E_Electric = -_DeltaU/(Math.Abs(q)*d*Math.Cos(Math.Rad(Teta)))
-			
-			Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
-			Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
-			Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
-			Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
-			Binesh.AddToHTML("E = ?", "V")
-			
-			Binesh.AddToHTML("E = - <span>"..Binesh.ProgressNumber(_DeltaU).."/|"..Binesh.ProgressNumber(q).."|*"..Binesh.ProgressNumber(d).."*Cos"..Binesh.ProgressNumber(Teta).."</span> &#8658; E = "..Binesh.ProgressNumber(E_Electric).." N/C")
+		
+		if E_Electric == nil and (ListBox.FindItem("Words", -1, LB_BYTEXT, "„Ìœ«‰") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "E") ~= -1) then
+			if q ~= nil and _DeltaU ~= nil and d ~= nil and Teta ~= nil then
+				Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "E = - <span>DeltaU/|q|dCosTeta</span>")
+				E_Electric = -_DeltaU/(Math.Abs(q)*d*Math.Cos(Math.Rad(Teta)))
+				
+				Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
+				Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
+				Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
+				Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
+				Binesh.AddToHTML("E = ?", "V")
+				
+				Binesh.AddToHTML("E = - <span>"..Binesh.ProgressNumber(_DeltaU).."/|"..Binesh.ProgressNumber(q).."|*"..Binesh.ProgressNumber(d).."*Cos"..Binesh.ProgressNumber(Teta).."</span> &#8658; E = "..Binesh.ProgressNumber(E_Electric).." N/C")
+			end
 		end
-	end
-	
-	if q == nil and (ListBox.FindItem("Words", -1, LB_BYTEXT, "–—Â") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "q") ~= -1) then
-		if E_Electric ~= nil and _DeltaU ~= nil and Teta ~= nil and d ~= nil then
-			Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "q = |<span>DeltaU/EdCosTeta</span>|")
-			
-			q = Math.Abs(_DeltaU/(E_Electric*d*Math.Cos(Math.Rad(Teta))))
-			
-			Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
-			Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
-			Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
-			Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
-			Binesh.AddToHTML("q = ?", "V")
-			
-			Binesh.AddToHTML("q = |<span>"..Binesh.ProgressNumber(_DeltaU).."/"..Binesh.ProgressNumber(E_Electric).."*"..Binesh.ProgressNumber(d).."*Cos"..Binesh.ProgressNumber(Teta).."</span>| &#8658; q = "..Binesh.ProgressNumber(q).." C")
+		
+		if q == nil and (ListBox.FindItem("Words", -1, LB_BYTEXT, "–—Â") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "q") ~= -1) then
+			if E_Electric ~= nil and _DeltaU ~= nil and Teta ~= nil and d ~= nil then
+				Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "q = |<span>DeltaU/EdCosTeta</span>|")
+				
+				q = Math.Abs(_DeltaU/(E_Electric*d*Math.Cos(Math.Rad(Teta))))
+				
+				Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
+				Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
+				Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
+				Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
+				Binesh.AddToHTML("q = ?", "V")
+				
+				Binesh.AddToHTML("q = |<span>"..Binesh.ProgressNumber(_DeltaU).."/"..Binesh.ProgressNumber(E_Electric).."*"..Binesh.ProgressNumber(d).."*Cos"..Binesh.ProgressNumber(Teta).."</span>| &#8658; q = "..Binesh.ProgressNumber(q).." C")
+			end
 		end
-	end
-	
-	if d == nil and ((ListBox.FindItem("Words", -1, LB_BYTEXT, "›«’·Â") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "›«’·Â") ~= -1) or (ListBox.FindItem("Words", -1, LB_BYTEXT, "d") ~= -1)) then 
-		if E_Electric ~= nil and _DeltaU ~= nil and Teta ~= nil and q ~= nil then
-			Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "d = - <span>DeltaU/E|q|CosTeta</span>")
-			d = -_DeltaU / (E_Electric*Math.Abs(q)*Math.Cos(Math.Rad(Teta)))
-			
-			Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
-			Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
-			Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
-			Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
-			Binesh.AddToHTML("d = ?", "V")
-			
-			Binesh.AddToHTML("d = - <span>"..Binesh.ProgressNumber(_DeltaU).."/"..Binesh.ProgressNumber(E_Electric).."*|"..Binesh.ProgressNumber(q).."|*Cos"..Binesh.ProgressNumber(Teta).."</span> &#8658; d = "..Binesh.ProgressNumber(d).." m")
+		
+		if d == nil and ((ListBox.FindItem("Words", -1, LB_BYTEXT, "›«’·Â") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "›«’·Â") ~= -1) or (ListBox.FindItem("Words", -1, LB_BYTEXT, "d") ~= -1)) then 
+			if E_Electric ~= nil and _DeltaU ~= nil and Teta ~= nil and q ~= nil then
+				Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "d = - <span>DeltaU/E|q|CosTeta</span>")
+				d = -_DeltaU / (E_Electric*Math.Abs(q)*Math.Cos(Math.Rad(Teta)))
+				
+				Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
+				Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
+				Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
+				Binesh.AddToHTML("Teta = "..Binesh.ProgressNumber(Teta), "V")
+				Binesh.AddToHTML("d = ?", "V")
+				
+				Binesh.AddToHTML("d = - <span>"..Binesh.ProgressNumber(_DeltaU).."/"..Binesh.ProgressNumber(E_Electric).."*|"..Binesh.ProgressNumber(q).."|*Cos"..Binesh.ProgressNumber(Teta).."</span> &#8658; d = "..Binesh.ProgressNumber(d).." m")
+			end
 		end
-	end
-	
-	if Teta == nil and (ListBox.FindItem("Words", -1, LB_BYTEXT, "“«ÊÌÂ") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "Teta") ~= -1) then
-		if E_Electric ~= nil and _DeltaU ~= nil and d ~= nil and q ~= nil then
-			Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "CosTeta = - <span>DeltaU/E|q|d</span>")
-			CosTeta = -_DeltaU/(E_Electric*Math.Abs(q)*d)
-			Teta_Radian = Math.Acos(Math.Acos(Math.Cos(CosTeta)))
-			Teta = Math.RadToDeg(Teta_Radian)
-			
-			Binesh.AddToHTML("CosTeta = - <span>"..Binesh.ProgressNumber(_DeltaU).."/"..Binesh.ProgressNumber(E_Electric).."*|"..Binesh.ProgressNumber(q).."|*"..Binesh.ProgressNumber(d).." </span> &#8658; CosTeta = "..Binesh.ProgressNumber(CosTeta).." &#8658; Teta<sub>Radian</sub> = "..Binesh.ProgressNumber(Teta_Radian).." &#8658; Teta = "..Binesh.ProgressNumber(Teta).."<sup>o</sup>")
-			
-			Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
-			Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
-			Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
-			Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
-			Binesh.AddToHTML("Teta = ?", "V")
+		
+		if Teta == nil and (ListBox.FindItem("Words", -1, LB_BYTEXT, "“«ÊÌÂ") ~= -1 or ListBox.FindItem("Words", -1, LB_BYTEXT, "Teta") ~= -1) then
+			if E_Electric ~= nil and _DeltaU ~= nil and d ~= nil and q ~= nil then
+				Binesh.CreateHTMLFile("DeltaU = -E|q|dCosTeta", "CosTeta = - <span>DeltaU/E|q|d</span>")
+				CosTeta = -_DeltaU/(E_Electric*Math.Abs(q)*d)
+				Teta_Radian = Math.Acos(Math.Acos(Math.Cos(CosTeta)))
+				Teta = Math.RadToDeg(Teta_Radian)
+				
+				Binesh.AddToHTML("CosTeta = - <span>"..Binesh.ProgressNumber(_DeltaU).."/"..Binesh.ProgressNumber(E_Electric).."*|"..Binesh.ProgressNumber(q).."|*"..Binesh.ProgressNumber(d).." </span> &#8658; CosTeta = "..Binesh.ProgressNumber(CosTeta).." &#8658; Teta<sub>Radian</sub> = "..Binesh.ProgressNumber(Teta_Radian).." &#8658; Teta = "..Binesh.ProgressNumber(Teta).."<sup>o</sup>")
+				
+				Binesh.AddToHTML("DeltaU = "..Binesh.ProgressNumber(_DeltaU).." J", "V")
+				Binesh.AddToHTML("E = "..Binesh.ProgressNumber(E_Electric).." N/C", "V")
+				Binesh.AddToHTML("q = "..Binesh.ProgressNumber(q).." C", "V")
+				Binesh.AddToHTML("d = "..Binesh.ProgressNumber(d).." m", "V")
+				Binesh.AddToHTML("Teta = ?", "V")
+			end
 		end
 	end
 end
